c98499564c67d48050ce494b358888292507317b
Phanlop Boonleua <phanlop.auto@gmail.com>
2026-07-27 11:10:10 +0700

    remove unused project

--- flutter_ui02_student_bio/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_ui02_student_bio
-
-A new Flutter project.
--- flutter_ui02_student_bio/android/app/src/main/kotlin/com/example/flutter_ui02_student_bio/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.flutter_ui02_student_bio
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
